00a737923ed500ec2185fc7efb0f0ceddfce6a79
TomasCardona07 <tomascardona787@gmail.com>
2026-05-22 00:58:05 -0500

    Syntaxis de JAVA
    primeros ejercicios en JAVA

---
+++ PRIMEROS PASOS/Condicionales.java
@@ -0,0 +1,15 @@
+public class Condicionales {
+
+    public static void main(String[] Condiciones) {
+        var nota = 4;
+        if (nota == 5){
+            System.out.println("Excelente");
+        }
+        else if (nota >= 3){
+            System.out.println("Pasaste");
+        }
+        else{
+            System.out.println("Reprobaste");
+        }
+    }
+}
---
+++ PRIMEROS PASOS/Variables.java
@@ -0,0 +1,6 @@
+public class Variables {
+    public static void main(String[] args){
+        var edad = 30; //el "var" declara el tipo de variable automaticamente segun su valor
+        System.out.println(edad); //mostrar por consola
+    }
+}
