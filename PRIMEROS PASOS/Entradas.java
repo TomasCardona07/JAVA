e15800f136515436de14c8083a4f8283946854ac
TomasCardona07 <tomascardona787@gmail.com>
2026-05-23 20:53:05 -0500

    Input del usuario
    Uso y creación del Scanner en JAVA

---
+++ PRIMEROS PASOS/Entradas.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; //Se importa el scanner para leer entradas
+public class Entradas {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in); //Variable del Scanner y creacion
+        System.out.println("Ingresa tu nombre");
+        var nombre = entrada.nextLine(); //Input del usuario, se guarda en la variable
+        System.out.println("Hola "+nombre);
+        entrada.close(); //cierre del Scanner
+    }
+}
+
+ 
