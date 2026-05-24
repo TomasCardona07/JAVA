fb2fcdbd392d3e86aad17b0b616a461e8a3d5777
TomasCardona07 <tomascardona787@gmail.com>
2026-05-24 13:37:12 -0500

    Notas de un estudiante
    Este programa pide y guarda 5 notas de un estudiante, saca el promedio y dice si aprobó o no dependiendo su promedio

---
+++ PRIMEROS PASOS/NotasEst.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class NotasEst {
+    public static void main(String[] args) {
+        Scanner entradas = new Scanner(System.in);
+        double[] notas = new double[5];
+        for (int i = 0; i < notas.length; i++){
+            System.out.println("ingrese la "+(i + 1)+ " nota del estudiante");
+            notas[i] = entradas.nextDouble();
+            while (notas[i] < 0){
+                System.out.println("La nota no puede ser negativa, ingresela nuevamente");
+                notas[i] = entradas.nextDouble();         
+            }
+        }
+        double acumulador = 0;
+        for (int i = 0; i < notas.length; i++){
+            acumulador = acumulador + notas[i];
+        }
+        double promedio = (acumulador / notas.length);
+        System.out.println("su promedio es: " + promedio);
+        if (promedio >= 3){
+            System.out.println("Aprobó la materia");
+        }
+        else{
+            System.out.println("Perdió la materia");
+        }
+        entradas.close();
+    }
+}
