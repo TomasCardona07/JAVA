e8d0f3e972297941b058b6bd524da81ed218bbca
TomasCardona07 <tomascardona787@gmail.com>
2026-06-04 23:54:59 -0500

    Comentando encapsulación

--- POO/encapsulación/Player.java
+++ POO/encapsulación/Player.java
@@ -1,21 +1,26 @@
 package encapsulación;
 
 public class Player {
+    //Clases privadas
     private String name;
     private int health;
+    //Constructor
     Player(String name,int health){
         this.name = name;
         this.health = health;
     }
+    //uso de getter para extraer health
     public int getHealth(){
         return health;
     }
+    //metodo para recibir la vida y calcular el daño
     public void receiveDamage(int damage){
         health -= damage;
     }
+    //Main
     public static void main(String[] args) {
         Player player1 = new Player("Tomas", 100);
         player1.receiveDamage(30);
-        System.out.println(player1.getHealth());
+        System.out.println(player1.getHealth()); //Se imprime el metodo del player1
     }
 }
