48a297749d14a0412b41c2dad462fabb0cbfa33f
TomasCardona07 <tomascardona787@gmail.com>
2026-06-07 16:59:32 -0500

    Inventario RPG terminado

--- POO/proyectos/inventarioRPG/Arma.java
+++ POO/proyectos/inventarioRPG/Arma.java
@@ -1,9 +1,12 @@
 package proyectos.inventarioRPG;
 
 public class Arma {
+    //Atributos de las armas
     String nombre;
     String tipo;
     int poder;
+
+    //Constructor
     Arma(String nombre, int poder, String tipo){
         this.nombre = nombre;
         this.tipo = tipo;
