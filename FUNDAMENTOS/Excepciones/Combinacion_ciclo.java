cb17c2c04f765b55d21e5f2397a5c229a2b002a9
Tomas <tomascardona787@gmail.com>
2026-06-26 23:38:52 -0500

    Combinando excepcion con ciclos

---
+++ FUNDAMENTOS/Excepciones/Combinacion_ciclo.java
@@ -0,0 +1,23 @@
+package Excepciones;
+import java.util.Scanner;
+public class Combinacion_ciclo {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        int num = -1;
+        while (num < 1){
+            try {
+                System.out.println("Ingresa un numero positivo");
+                num = Integer.parseInt(scr.nextLine());
+                if (num < 1 ) {
+                    System.out.println("Numero incorrecto");
+                }
+                else{
+                    System.out.println("Felicidades");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Ingrese un numero por favor");
+            }
+        }   
+        scr.close();
+    }
+}
