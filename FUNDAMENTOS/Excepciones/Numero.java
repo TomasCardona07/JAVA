b320c078d3e63e81cbd86b84f3bae7da9e0927f6
TomasCardona07 <tomascardona787@gmail.com>
2026-06-30 23:49:34 -0500

    Uso de excepciones
    Added a comment to clarify the purpose of the try block.

--- FUNDAMENTOS/Excepciones/Numero.java
+++ FUNDAMENTOS/Excepciones/Numero.java
@@ -4,7 +4,7 @@ public class Numero {
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
         int num;
-        try {
+        try { //Se ejecuta una accion
             System.out.println("Ingresa un numero");
             num = Integer.parseInt(scr.nextLine());
         } catch (NumberFormatException e) { // NumberFormatException: excepción de Strings en datos numéricos
